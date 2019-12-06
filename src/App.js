a830db69485454bbca72c335d80a0fba08134810
Samson Kolge <samson.kolge@walmartlabs.com>
2019-12-06 09:45:07 +0530

    HOC addition

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
 import './App.css';
 import Homepage from './pages/homepage/homepage.component';
-
+import ShopPage from './pages/shop/shop.component';
 
 function App() {
   return (
     <div>
-      <Homepage/>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path='/' component={Homepage}/>
+          <Route path='/shop' component={ShopPage}/>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
